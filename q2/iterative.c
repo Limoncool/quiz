2e0f2f14568bd7e5edcdcf4b7922e602418bd831
Limoncool <tsaiwk2006@yahoo.com.tw>
2015-10-18 04:27:08 +0800

    Upload the own version of the q2 code of homework 3.
    using easy character comparison to solve the question.
    Always recording the smallest chararcter at first,if no chararcter fits the condition,then give the value of the variable "smallest" to the output directly.
    Upload both the iterative and recursive versions.
    Adding an input file test1.txt, needed to input "./(executible) test1.txt" when execute the program.

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,81 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+char smallest_character (char[], char);
+
+int main(int argc, char *argv[])
 {
-    return 0;
+char tempInput[100];
+char searchChar;
+char temp;
+char output;
+int count = 0;
+int end = 0;
+
+FILE *input;
+input = fopen (argv[1], "r");
+if (input == NULL) {
+	printf("FILE NOT FOUND!!");
+	exit(1);
 }
+
+while (fscanf (input, "%c", &temp) != EOF) {
+	if (temp == ']') {
+		end = 1;
+	}
+	else if (temp != '[' && temp != '\'' && temp != ',' && temp != ' ' && temp != '\n') {
+		switch(end) {
+			case 0:
+				tempInput[count] = temp;
+				count++;
+				break;
+			case 1:
+				searchChar = temp;
+				end = 0;
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+char inputString[count];
+int eco;
+for (eco = 0;eco < count;eco++) {
+	inputString[eco] = tempInput[eco];
+}
+
+output = smallest_character(inputString, searchChar);
+printf("The smallest character is '%c'.\n", output);
+
+
+fclose(input);
+return 0;
+}
+
+
+char smallest_character (char str[], char c) {
+int length = strlen(str);
+char smallest = NULL, result = NULL;
+int a;
+
+for (a = 0;a < length;a++) {
+	if (smallest == NULL || str[a] < smallest) {
+		smallest = str[a];
+	}
+
+	if (str[a] > c) {
+		if (result == NULL || str[a] < result) {
+			result = str[a];
+		}
+	}	
+}
+
+if (result == NULL) {
+	result = smallest;
+}
+
+return result;
+}
+
--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,87 @@
-/* FIXME: Implement! */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main()
+char inputString[100];
+char searchChar;
+
+int length;
+char smallest;
+char output;
+int a = 0;
+
+char smallest_character ();
+
+int main(int argc, char *argv[])
 {
-    return 0;
+char temp;
+char output = ' ';
+int count = 0;
+int end = 0;
+
+FILE *input;
+input = fopen (argv[1], "r");
+if (input == NULL) {
+	printf("FILE NOT FOUND!!");
+	exit(1);
+}
+
+while (fscanf (input, "%c", &temp) != EOF) {
+	if (temp == ']') {
+		end = 1;
+	}
+	else if (temp != '[' && temp != '\'' && temp != ',' && temp != ' ' && temp != '\n') {
+		switch(end) {
+			case 0:
+				inputString[count] = temp;
+				count++;
+				break;
+			case 1:
+				searchChar = temp;
+				end = 0;
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+length = count;
+
+output = smallest_character();
+printf("The smallest character is '%c'.\n", output);
+
+fclose(input);
+
+return 0;
+}
+
+
+char smallest_character () { 
+if (smallest == NULL || inputString[a] < smallest) {
+	smallest = inputString[a];
+}
+
+if (inputString[a] > searchChar) {
+	if (output == NULL || inputString[a] < output) {
+		output = inputString[a];
+	}
+}	
+
+
+if (a < length - 1) {
+	a++;
+}
+else if (output == NULL) {
+	output = smallest;
+	return output;
+}
+else {
+	return output;
+}
+
+
+smallest_character();
+
+return output;
 }
